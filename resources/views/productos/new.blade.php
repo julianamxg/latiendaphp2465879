038ebbebcab16918f1c55cbb4ae80e398eb985ec
DanielDSanchez <86128586+DanielDSanchez@users.noreply.github.com>
2022-05-10 11:52:40 -0500

    layouts y migraciones

---
+++ resources/views/productos/new.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.menu')
+
+@section('contenido')
+<div class="row">
+    <h1 class="light-green-text text-darken-3">Nuevo producto</h1>
+</div>
+
+<div class="row">
+    <form class="col s8" method="post" action="">
+       <div class="row">
+           <div class="input-field col s8">
+           <input placeholder="Nombre del producto" id="nombre" name="nombre" type="text">
+           <label class="light-green-text text-darken-3" for="nombre">Nombre del producto</label>
+           </div>
+       </div>
+
+       <div class="row">
+          <div class="input-field col s8">
+            <textarea name="desc" id="desc" class="materialize-textarea"></textarea>
+            <label class="light-green-text text-darken-3" for="desc">Descripción del producto</label>
+          </div>
+       </div>
+
+       <div class="row">
+           <div class="input-field col s8">
+               <input placeholder="Precio del producto" id="precio" name="precio" type="text">
+               <label class="light-green-text text-darken-3" for="precio">Precio del producto</label>
+           </div>
+       </div>
+<div class="row">
+       <div class="file-field input-field col s8">
+      <div class="btn green lighten-3">
+        <span>Imagen del producto</span>
+        <input type="file" name="imagen">
+      </div>
+      <div class="file-path-wrapper">
+        <input class="file-path validate" type="text">
+      </div>
+    </div>
+    </div>
+    
+
+    <div class="row">
+        <button class="waves-effect waves-light green lighten-3 btn " type="submit" name="action">Guardar</button>
+    </div>
+    </form>
+
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -68,3 +68,7 @@ Route::get('paises' , function(){
    return view('paises')->with("paises", $paises);
 
 });
+
+Route::get('prueba', function(){
+    return view('productos.new');
+});
